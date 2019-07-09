15df5849fb3e3559ef180f48a6456a4ddcfa33a5
rakesh635 <rakesh635@gmail.com>
2019-07-09 14:31:12 +0530

    Update testdsl.groovy

--- testdsl.groovy
+++ testdsl.groovy
@@ -1,3 +1,14 @@
+job('test-job') {
+    scm {
+        git('git://github.com/quidryan/aws-sdk-test.git')
+    }
+    triggers {
+        scm('*/15 * * * *')
+    }
+    steps {
+        maven('-e clean test')
+    }
+}
 job('DSL-Tutorial-1-Test') {
     scm {
         git('git://github.com/quidryan/aws-sdk-test.git')
