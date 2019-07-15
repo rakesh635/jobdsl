d3ff83ea114c82fefdca3dcb6c1c616a071693b6
rakesh635 <rakesh635@gmail.com>
2019-07-15 17:46:46 +0530

    Update testdsl.groovy

--- testdsl.groovy
+++ testdsl.groovy
@@ -1,14 +1,3 @@
-job('test-job') {
-    scm {
-      git("$repourl")
-    }
-    triggers {
-        scm('*/15 * * * *')
-    }
-    steps {
-        maven('-e clean test')
-    }
-}
 job('DSL-Tutorial-1-Test') {
     scm {
         git("$repourl")
@@ -18,5 +7,25 @@ job('DSL-Tutorial-1-Test') {
     }
     steps {
         maven('-e clean test')
+    }
+}
+pipelineJob("$pipelinename")
+{
+    def repo = "$repourl"
+    triggers {
+        scm('H/5 * * * *')
+    }
+    description("Pipeline for $repo")
+    definition {
+      cpsScm {
+          scm {
+              git {
+                  remote { url(repo) }
+                  branches('master', '**/feature*')
+                  scriptPath('misc/Jenkinsfile.v2')
+                  extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+              }
+          }
+      }
     }
 }
