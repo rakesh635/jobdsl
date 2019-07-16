9d3c1148896264f247e2308551cff102f2433091
rakesh635 <rakesh635@gmail.com>
2019-07-16 11:50:03 +0530

    Update testdsl.groovy

--- testdsl.groovy
+++ testdsl.groovy
@@ -17,15 +17,15 @@ pipelineJob("$pipelinename")
     }
     description("Pipeline for $repo")
     definition {
-      cpsScm {
-          scm {
-              git {
-                  remote { url(repo) }
-                  branches('master', '**/feature*')
-                  scriptPath('misc/Jenkinsfile.v2')
-                  extensions { }  // required as otherwise it may try to tag the repo, which you may not want
-              }
-          }
-      }
+        cpsScm {
+            scm {
+                git {
+                    remote { url(repo) }
+                    branches('master', '**/feature*')
+                    scriptPath('misc/Jenkinsfile.v2')
+                    extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+                }
+            }
+        }
     }
 }
