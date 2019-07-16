c39b5f057b3098782f7abc9df28b27e622d65e9a
rakesh635 <rakesh635@gmail.com>
2019-07-16 14:01:27 +0530

    Update testdsl.groovy

--- testdsl.groovy
+++ testdsl.groovy
@@ -1,18 +1,25 @@
-pipelineJob("$pipelinename")
+if("$LANG" == "PHP" || "$LANG" == "php")
 {
-    def repo = "$repourl"
-    triggers {
-        scm('H/5 * * * *')
-    }
-    description("Pipeline for $repo")
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote { url(repo) }
-                    branches('master', '**/feature*')
-                    scriptPath('misc/Jenkinsfile.v2')
-                    extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+    
+}
+elseif("$LANG" == "Java" || "$LANG" == "JAVA" || "$LANG" == "java")
+{
+    pipelineJob("$pipelinename")
+    {
+        def repo = "$repourl"
+        triggers {
+            scm('H/5 * * * *')
+        }
+        description("Pipeline for $repo")
+        definition {
+            cpsScm {
+                scm {
+                    git {
+                        remote { url(repo) }
+                        branches('master', '**/feature*')
+                        scriptPath('misc/Jenkinsfile.v2')
+                        extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+                    }
                 }
             }
         }
