72949e0599472faf0b5f795bd4f8d098370950cc
rakesh635 <rakesh635@gmail.com>
2019-07-16 15:28:29 +0530

    Update testdsl.groovy

--- testdsl.groovy
+++ testdsl.groovy
@@ -5,7 +5,10 @@ if("$LANG" == "PHP" || "$LANG" == "php")
 else if("$LANG" == "Java" || "$LANG" == "JAVA" || "$LANG" == "java")
 {
     def content = readFileFromWorkspace('java/Jenkinsfile')
+    println(content);
     content.replaceAll('--REPOURL--',"$repourl")
+    println(content);
+    println("$repourl");
     pipelineJob("$pipelinename")
     {
         def repo = "$repourl"
